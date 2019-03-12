c97afa1826def3eae895e62109736ae6db6076f0
egouge <egouge@refractions.net>
2019-03-12 16:11:52 -0700

    added copyright to all java files

--- chyf-web/src/test/java/net/refractions/chyf/hygraph/PourpointTest.java
+++ chyf-web/src/test/java/net/refractions/chyf/hygraph/PourpointTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2019 Government of Canada
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
 package net.refractions.chyf.hygraph;
 
 import java.io.IOException;
@@ -22,7 +37,6 @@ import net.refractions.chyf.ChyfDatastore;
 import net.refractions.chyf.pourpoint.Pourpoint;
 import net.refractions.chyf.pourpoint.PourpointEngine;
 import net.refractions.chyf.pourpoint.PourpointOutput;
-import net.refractions.chyf.pourpoint.UniqueSubCatchment;
 import net.refractions.chyf.rest.GeotoolsGeometryReprojector;
 
 /**
